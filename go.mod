510dc360216c564832623294cd34482f06a12003
Colton McCurdy <mccurdyc22@gmail.com>
2020-01-03 09:25:11 -0500

    traces with correlated logs
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- go.mod
+++ go.mod
@@ -5,8 +5,11 @@ go 1.13
 require (
 	github.com/DataDog/datadog-go v3.3.1+incompatible // indirect
 	github.com/gin-gonic/gin v1.5.0
+	github.com/google/uuid v1.1.1
 	github.com/opentracing/opentracing-go v1.1.0 // indirect
 	github.com/philhofer/fwd v1.0.0 // indirect
+	github.com/rakyll/hey v0.1.2 // indirect
+	github.com/sirupsen/logrus v1.4.2
 	github.com/tinylib/msgp v1.1.1 // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 	gopkg.in/DataDog/dd-trace-go.v1 v1.19.0
--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
+	"io/ioutil"
+	"math/rand"
 	"os"
+	"time"
 
 	gintrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gin-gonic/gin"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 )
 
+var ids = map[int]uuid.UUID{
+	0: uuid.New(),
+	1: uuid.New(),
+	2: uuid.New(),
+	3: uuid.New(),
+	4: uuid.New(),
+	5: uuid.New(),
+}
+
 func main() {
+	f, err := os.OpenFile("/var/log.log", os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		logrus.Fatalf("failed to open log file")
+	}
+	logrus.SetOutput(f)
+
 	// https://godoc.org/gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer
 	tracer.Start(
 		tracer.WithAnalytics(true),
@@ -23,9 +43,14 @@ func main() {
 
 	// Create a gin.Engine
 	r := gin.New()
+	gin.DefaultWriter = ioutil.Discard
 
 	// Use the tracer middleware with your desired service name.
 	r.Use(gintrace.Middleware("go-sample"))
+
+	// https://docs.datadoghq.com/logs/log_collection/go/#configure-your-logger
+
+	logrus.SetFormatter(&logrus.JSONFormatter{})
 
 	// Continue using the router as normal.
 	r.GET("/hello", func(c *gin.Context) {
@@ -37,10 +62,30 @@ func main() {
 		traceID := span.Context().TraceID()
 		spanID := span.Context().SpanID()
 
-		// Append them to log messages as fields:
-		log.Printf("my log message dd.trace_id=%d dd.span_id=%d", traceID, spanID)
-		c.String(404, "Hello World!")
+		rnd := rand.Intn(5)
+		span.SetTag("sku_uuid", ids[rnd])
+
+		// https://docs.datadoghq.com/tracing/advanced/connect_logs_and_traces/?tab=go
+		// for now, you manually have to add these fields in Go for them to
+		// be tied into DD log collection
+		logrus.WithFields(logrus.Fields{"sku_uuid": ids[rnd], "dd.trace_id": traceID, "dd.span_id": spanID}).Infof("my log message")
+
+		ctx := tracer.ContextWithSpan(context.Background(), span)
+		wait(ctx, rnd, time.Second*2)
+
+		c.String(200, "Hello World!")
 	})
 
 	r.Run(":8080")
+}
+
+func wait(ctx context.Context, fanout int, dur time.Duration) {
+	span, ctx := tracer.StartSpanFromContext(ctx, "wait", tracer.ResourceName("wait"))
+	defer span.Finish()
+
+	for i := 0; i < fanout; i++ {
+		innerSpan := tracer.StartSpan("innerwait", tracer.ResourceName("innerwait"), tracer.ChildOf(span.Context()))
+		time.Sleep(dur)
+		innerSpan.Finish()
+	}
 }
